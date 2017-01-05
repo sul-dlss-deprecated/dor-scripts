df066bef54bff2e53a6596d652ffed10278912dd
Chris Beer <chris@cbeer.info>
2017-01-05 09:54:30 -0800

    Clean up some rubocop violations

--- bin/20170104_remediate_duplicate_contentmetadata_resource_ids.rb
+++ bin/20170104_remediate_duplicate_contentmetadata_resource_ids.rb
@@ -15,7 +15,7 @@ OptionParser.new do |opts|
     </contentMetadata>
   EOB
 
-  opts.on("-h", "--help", "Prints this help") do
+  opts.on('-h', '--help', 'Prints this help') do
     puts opts
     exit
   end
@@ -27,14 +27,14 @@ remediate do
 
   # Only process records with missing or non-unique resource ids
   condition do |obj|
-    ids = obj.contentMetadata.ng_xml.xpath('//resource/@id').map { |id| id.to_s }
+    ids = obj.contentMetadata.ng_xml.xpath('//resource/@id').map(&:to_s)
 
     ids.any? { |id| id.nil? || id.empty? } || ids.length != ids.uniq.length
   end
 
   each_druid do
     open_new_version
-    
+
     contentMetadata.ng_xml_will_change!
     contentMetadata.ng_xml.xpath('//resource').each_with_index do |r, i|
       r.attributes['id'].value = "#{bare_druid}_#{i + 1}"
--- bin/decommission.rb
+++ bin/decommission.rb
@@ -4,13 +4,13 @@ require_relative '../boot.rb'
 args = Struct.new(:tag).new
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: decommision.rb [options] [FILE]"
+  opts.banner = 'Usage: decommision.rb [options] [FILE]'
 
   opts.on('-tTAG', '--tag=TAG', 'Decommission tag') do |t|
     args.tag = t
   end
 
-  opts.on("-h", "--help", "Prints this help") do
+  opts.on('-h', '--help', 'Prints this help') do
     puts opts
     exit
   end
@@ -20,7 +20,7 @@ remediate do
   each_druid do
     decommission(args.tag)
     save
-    
+
     Dor::DigitalStacksService.prune_stacks_dir druid
     publish_metadata
     Dor::CleanupService.cleanup_by_druid druid
